addd01fda4712d1004872e963a7d29df2f05094e
Gitea <frvzmb@gmail.com>
2019-10-29 09:57:33 +0300

    remove webpack addon determination

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./config/webpack.common.config');
 
 module.exports = (env) => {
-  const determineAddons = (addons) => [...[addons]]
-    .filter((addon) => Boolean(addon))
-    .map((addon) => require(`./config/addons/webpack.${addon}.js`)); // eslint-disable-line import/no-dynamic-require,global-require
-
   const envConfig = require(`./config/webpack.${env.env}.config`); // eslint-disable-line import/no-dynamic-require,global-require
-
-  return webpackMerge(commonConfig, envConfig, ...determineAddons(env.addons));
+  return webpackMerge(commonConfig, envConfig);
 };
