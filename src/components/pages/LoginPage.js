dfc768a9fa13a78cbf29760f6f09c8db8f37c519
Alexander Emelin <frvzmb@gmail.com>
2020-09-15 10:21:22 +0300

    remove github ribbon

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -263,9 +263,6 @@ export default class LoginPage extends React.Component {
 
     return (
       <div className={loginClasses}>
-        <a href="https://github.com/centrifugal/centrifugo" rel="noopener noreferrer" target="_blank">
-          <img className="login-forkme" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" />
-        </a>
         <div className="login-body">
           <div className="container">
             <div className="row">
