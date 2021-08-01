1b32020b347250f6c8f65bddc644505ece67531c
Alexander Emelin <frvzmb@gmail.com>
2021-08-01 22:48:03 +0300

    num subs

--- src/components/pages/StatusPage.js
+++ src/components/pages/StatusPage.js
@@ -37,9 +37,10 @@ export default class StatusPage extends React.Component {
                   <th title="Node name">Node name</th>
                   <th title="Node version">Version</th>
                   <th title="Node uptime">Uptime</th>
-                  <th title="Total active channels on node">Channels</th>
                   <th title="Total connected clients on node">Clients</th>
                   <th title="Total unique clients on node">Users</th>
+                  <th title="Total active subscriptions on node">Subs</th>
+                  <th title="Total unique channels on node">Channels</th>
                   <th title="Node CPU usage">CPU %</th>
                   <th title="Node RSS memory usage">RSS</th>
                 </tr>
@@ -79,6 +80,7 @@ class NodeRow extends React.Component {
     const {
       name, version, uptime, num_channels: numChannels,
       num_clients: numClients, num_users: numUsers,
+      num_subs: numSubs,
       process,
     } = node;
 
@@ -87,9 +89,10 @@ class NodeRow extends React.Component {
         <td>{name}</td>
         <td>{version}</td>
         <td>{HumanSeconds(uptime)}</td>
-        <td>{numChannels}</td>
         <td>{numClients}</td>
         <td>{numUsers}</td>
+        <td>{numSubs}</td>
+        <td>{numChannels}</td>
         <td>{process ? (process.cpu || 0).toFixed(1) : 'n/a'}</td>
         <td>{process ? HumanSize(process.rss) : 'n/a'}</td>
       </tr>
