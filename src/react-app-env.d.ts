d291e2a76d0d702f23ae52436fcfea29d9fee6f5
Alexander Emelin <frvzmb@gmail.com>
2022-10-05 16:27:50 +0300

    work

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="react-scripts" />
-
-// TODO: Contribute this to Trystero
-declare module 'trystero' {
-  export type RoomConfig = BaseRoomConfig &
-    (BitTorrentRoomConfig | FirebaseRoomConfig | IpfsRoomConfig)
-}
