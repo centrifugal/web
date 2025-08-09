13afc566876bef77bd006a6585ad92f94074fb25
FZambia <frvzmb@gmail.com>
2025-08-09 17:21:17 +0300

    disabled login button, download tracing log

--- src/components/Shell/Shell.test.tsx
+++ src/components/Shell/Shell.test.tsx
@@ -1,25 +1,38 @@
 import { render } from '@testing-library/react'
 import { MemoryRouter as Router } from 'react-router-dom'
+import { AuthProvider } from 'react-oidc-context'
+import { act } from '@testing-library/react'
 
 import { Shell, ShellProps } from './Shell'
 
 const ShellStub = (overrides: Partial<ShellProps> = {}) => {
   return (
-    <Router>
-      <Shell
-        handleLogin={(password: string) => {}}
-        handleLogout={() => {}}
-        passwordAuthenticated={true}
-        edition={'oss'}
-        {...overrides}
-      />
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <AuthProvider>
+        <Shell
+          handleLogin={(password: string) => {}}
+          handleLogout={() => {}}
+          passwordAuthenticated={true}
+          edition={'oss'}
+          {...overrides}
+        />
+      </AuthProvider>
     </Router>
   )
 }
 
 describe('Shell', () => {
-  test('can be opened', () => {
-    const { container } = render(<ShellStub />)
-    expect(container).toBeInTheDocument()
+  test('can be opened', async () => {
+    let container: HTMLElement
+    await act(async () => {
+      const result = render(<ShellStub />)
+      container = result.container
+    })
+    expect(container!).toBeInTheDocument()
   })
 })
